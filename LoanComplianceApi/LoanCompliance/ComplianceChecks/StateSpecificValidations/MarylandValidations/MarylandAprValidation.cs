b4ed5962c94e3fe4886ab6400f9c96dd14343f97
Zach Sexton <zsexton2011@gmail.com>
2021-05-07 23:53:31 -0400

    Add Maryland APR validation

---
+++ LoanComplianceApi/LoanCompliance/ComplianceChecks/StateSpecificValidations/MarylandValidations/MarylandAprValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanComplianceApi.LoanCompliance.ComplianceChecks.StateSpecificValidations.MarylandValidations
+{
+    public class MarylandAprValidation : AprValidatorBase
+    {
+        private const decimal Rate = 4.00m;
+
+        public override State State { get; } = State.MD;
+
+        public override ComplianceCheck Validate(Loan loan)
+        {
+            ComplianceCheck.Passed = loan.Apr < Rate ? true : false;
+            return ComplianceCheck;
+        }
+    }
+}
