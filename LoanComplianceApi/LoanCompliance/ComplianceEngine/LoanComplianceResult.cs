4ac6dc6663bc25dfb17fb53ae8fc4333c5f92e0b
Zach Sexton <zsexton2011@gmail.com>
2021-05-08 00:11:02 -0400

    Override GetHashCode() for simple equality checking of LoanComplianceResult

--- LoanComplianceApi/LoanCompliance/ComplianceEngine/LoanComplianceResult.cs
+++ LoanComplianceApi/LoanCompliance/ComplianceEngine/LoanComplianceResult.cs
@@ -9,5 +9,23 @@ namespace LoanComplianceApi.LoanCompliance
     {
         public bool IsCompliant { get; set; }
         public List<ComplianceCheck> ComplianceChecks { get; set; }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            hash = hash * 23 + IsCompliant.GetHashCode();
+            if (ComplianceChecks != null || ComplianceChecks.Count != 0)
+            {
+                var complianceChecks = ComplianceChecks.OrderBy(x => x.ComplianceType);
+                foreach (var complianceCheck in complianceChecks)
+                {
+                    hash = hash * 23 + complianceCheck.ComplianceType.GetHashCode();
+                    hash = hash * 23 + complianceCheck.Passed.GetHashCode();
+                }
+            }
+           
+
+            return hash;
+        }
     }
 }
